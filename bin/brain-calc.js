b4354a25e51a572da74c1e56e82ece93b3ca6fbb
komarik <yaroslavedudin@yandex.ru>
2023-12-18 12:57:07 +0300

    the end

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,50 +1,50 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 import { name } from "../src/cli.js";
 const getRandomInt = (min, max) => {
   const lower = Math.ceil(min);
   const upper = Math.floor(max);
   return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 };
-name
-
+name;
 
 const generateQuestion = () => {
   const num1 = getRandomInt(1, 100);
   const num2 = getRandomInt(1, 100);
-  const operators = ['+', '-', '*'];
+  const operators = ["+", "-", "*"];
   const operator = operators[getRandomInt(0, operators.length - 1)];
   const question = `${num1} ${operator} ${num2}`;
   return question;
 };
 
 const calculate = (question) => {
-    const [num1, operator, num2] = question.split(' ');
-    switch (operator) {
-      case '+':
-        return parseInt(num1) + parseInt(num2);
-      case '-':
-        return parseInt(num1) - parseInt(num2);
-      case '*':
-        return parseInt(num1) * parseInt(num2);
-      default:
-        return null; 
-    }
-  };
-  
+  const [num1, operator, num2] = question.split(" ");
+  switch (operator) {
+    case "+":
+      return parseInt(num1) + parseInt(num2);
+    case "-":
+      return parseInt(num1) - parseInt(num2);
+    case "*":
+      return parseInt(num1) * parseInt(num2);
+    default:
+      return null;
+  }
+};
 
 const playCalculator = (name) => {
-  console.log('What is the result of the expression?');
+  console.log("What is the result of the expression?");
   let correctAnswersCount = 0;
   while (correctAnswersCount < 3) {
     const question = generateQuestion();
     console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
+    const userAnswer = readlineSync.question("Your answer: ");
     const correctAnswer = calculate(question);
     if (parseInt(userAnswer, 10) === correctAnswer) {
-      console.log('Correct!');
+      console.log("Correct!");
       correctAnswersCount += 1;
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
+      );
       console.log(`Let's try again, ${name}!`);
       break;
     }
@@ -53,5 +53,5 @@ const playCalculator = (name) => {
     console.log(`Congratulations, ${name}!`);
   }
 };
-console.log(playCalculator(name))
+console.log(playCalculator(name));
 export default playCalculator;
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import
-// eslint-disable-next-line import/no-useless-path-segments
-{ name } from '../src/cli.js';
+import readlineSync from "readline-sync";
+import {
+  // eslint-disable-next-line import/no-useless-path-segments
+  name,
+} from "../src/cli.js";
 
-console.log('Find the greatest common divisor of given numbers.');
+console.log("Find the greatest common divisor of given numbers.");
 let point = 0;
 
 for (let i = 0; i <= 2; i += 1) {
-  const randomNumberGen = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+  const randomNumberGen = (min, max) =>
+    Math.floor(Math.random() * (max - min)) + min;
   const rNum1 = randomNumberGen(1, 100);
   const rNum2 = randomNumberGen(1, 100);
   let answer = 0;
@@ -21,14 +23,16 @@ for (let i = 0; i <= 2; i += 1) {
   }
 
   console.log(`Question: ${rNum1} ${rNum2}`);
-  answer = (hcf);
-  let question = readlineSync.question('Your answer: ');
+  answer = hcf;
+  let question = readlineSync.question("Your answer: ");
   question = Number(question);
   if (question === answer) {
-    console.log('Correct!');
+    console.log("Correct!");
     point += 1;
   } else {
-    console.log(`'${question}' is wrong answer ;(. Correct answer was '${answer}'.`);
+    console.log(
+      `'${question}' is wrong answer ;(. Correct answer was '${answer}'.`
+    );
     console.log(`Let's try again, ${name}!`);
     break;
   }
