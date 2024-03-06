cb00a400b212b081dfc81a4888f8dfd869c0f352
komarik <yaroslavedudin@yandex.ru>
2024-03-06 15:31:35 +0300

    final round

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,20 +1,32 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import greeting from '../src/cli.js';
 
-import { playGame, getRandomNumber } from './utils.js';
-
-const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+greeting();
 
 const isEven = (num) => num % 2 === 0;
-
-const generateRound = () => {
-  const question = getRandomNumber(1, 100);
-  const correctAnswer = isEven(question) ? 'yes' : 'no';
-  return [question, correctAnswer];
-};
-
+const generateQuestion = () => Math.floor(Math.random() * 10);
 const startBrainEven = () => {
-  playGame(rules, generateRound);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let points = 0;
+
+  for (let i = 0; i <= 2; i += 1) {
+    const randomNumber = generateQuestion();
+    console.log(`Question: ${randomNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if ((isEven(randomNumber) && userAnswer.toLowerCase() === 'yes') || (!isEven(randomNumber) && userAnswer.toLowerCase() === 'no')) {
+      console.log('Correct!');
+      points += 1;
+    } else {
+      console.log(`'${userAnswer}' is the wrong answer ;(. Correct answer was '${isEven(randomNumber) ? 'no' : 'yes'}'.`);
+      console.log(`Let's try again, ${greeting}!`);
+      break;
+    }
+    if (points === 3) {
+      console.log(`Congratulations, ${greeting}!`);
+    }
+  }
 };
 
-startBrainEven();
 startBrainEven();
